0381617986efda9eceb3b7322294d881cc9a53b0
Artur Kutsevol <arturkutsevol@Arturs-MacBook-Air.local>
2022-10-03 21:37:43 +0300

    Update webhook

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ API_TOKEN = os.environ["TELEGRAM_TOKEN"]
 
 # webhook settings
 WEBHOOK_HOST = os.environ["WEBHOOK_HOST"]
-WEBHOOK_PATH = os.environ["WEBHOOK_PATH"]
+WEBHOOK_PATH = f'/webhook/{API_TOKEN}'
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 # webserver settings
@@ -28,15 +28,11 @@ dp.middleware.setup(LoggingMiddleware())
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    # Regular request
-    # await bot.send_message(message.chat.id, message.text)
-
-    # or reply INTO webhook
-    return SendMessage(message.chat.id, message.text)
+    await message.answer(message.text)
 
 
 async def on_startup(dp):
-    await bot.set_webhook(WEBHOOK_URL)
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
     # insert code here to run it after start
 
 
@@ -60,6 +56,7 @@ if __name__ == '__main__':
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
         on_startup=on_startup,
+        on_shutdown=on_shutdown,
         skip_updates=True,
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
