a8db62f6b4d3b5edbddb970596f49a4245b9fb56
Artur Kutsevol <arturkutsevol@Arturs-MacBook-Air.local>
2022-10-03 21:11:29 +0300

    Remove on_shutdown

--- bot.py
+++ bot.py
@@ -60,7 +60,6 @@ if __name__ == '__main__':
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
         on_startup=on_startup,
-        on_shutdown=on_shutdown,
         skip_updates=True,
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
