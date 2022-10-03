605ca497fbac35d583f4af71733bde26d149b341
Artur Kutsevol <arturkutsevol@Arturs-MacBook-Air.local>
2022-10-03 22:29:02 +0300

    Remove on shutdown

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 # webserver settings
 WEBAPP_HOST = os.environ["WEBAPP_HOST"]
-WEBAPP_PORT = os.environ["WEBAPP_PORT"]
+WEBAPP_PORT = int(os.environ["WEBAPP_PORT"])
 
 logging.basicConfig(level=logging.INFO)
 
@@ -36,27 +36,11 @@ async def on_startup(dp):
     # insert code here to run it after start
 
 
-async def on_shutdown(dp):
-    logging.warning('Shutting down..')
-
-    # insert code here to run it before shutdown
-
-    # Remove webhook (not acceptable in some cases)
-    await bot.delete_webhook()
-
-    # Close DB connection (if used)
-    await dp.storage.close()
-    await dp.storage.wait_closed()
-
-    logging.warning('Bye!')
-
-
 if __name__ == '__main__':
     start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
         on_startup=on_startup,
-        on_shutdown=on_shutdown,
         skip_updates=True,
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
