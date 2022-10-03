709269ea7726807c895507a571befc0a8f8696a5
Artur Kutsevol <arturkutsevol@Arturs-MacBook-Air.local>
2022-10-03 18:58:17 +0300

    Add webhook

--- bot.py
+++ bot.py
@@ -1,53 +1,66 @@
-"""
-This is a echo bot.
-It echoes any incoming text messages.
-"""
-import os
 import logging
 
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher.webhook import SendMessage
+from aiogram.utils.executor import start_webhook
+
 
 API_TOKEN = os.environ["TELEGRAM_TOKEN"]
 
-# Configure logging
+# webhook settings
+WEBHOOK_HOST = os.environ["WEBHOOK_HOST"]
+WEBHOOK_PATH = os.environ["WEBHOOK_PATH"]
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+# webserver settings
+WEBAPP_HOST = os.environ["WEBAPP_HOST"]
+WEBAPP_PORT = os.environ["WEBAPP_PORT"]
+
 logging.basicConfig(level=logging.INFO)
 
-# Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-
-
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
-
-
-@dp.message_handler(regexp='(^cat[s]?$|puss)')
-async def cats(message: types.Message):
-    with open('data/cats.jpg', 'rb') as photo:
-        '''
-        # Old fashioned way:
-        await bot.send_photo(
-            message.chat.id,
-            photo,
-            caption='Cats are here 😺',
-            reply_to_message_id=message.message_id,
-        )
-        '''
-
-        await message.reply_photo(photo, caption='Cats are here 😺')
+dp.middleware.setup(LoggingMiddleware())
 
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    # old style:
+    # Regular request
     # await bot.send_message(message.chat.id, message.text)
 
-    await message.answer(message.text)
+    # or reply INTO webhook
+    return SendMessage(message.chat.id, message.text)
+
+
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+    # insert code here to run it after start
+
+
+async def on_shutdown(dp):
+    logging.warning('Shutting down..')
+
+    # insert code here to run it before shutdown
+
+    # Remove webhook (not acceptable in some cases)
+    await bot.delete_webhook()
+
+    # Close DB connection (if used)
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+    logging.warning('Bye!')
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
